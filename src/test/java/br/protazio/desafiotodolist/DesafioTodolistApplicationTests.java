471df8fae13be6250c5a4a20f81c4187a1807980
franklinprotazio <protaziolancamentos@gmail.com>
2023-11-26 17:48:51 -0300

    primeiro commit

---
+++ src/test/java/br/protazio/desafiotodolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.protazio.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
